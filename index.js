e8a3cc39def307364c25582324379b479a10f5bc
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-17 05:55:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"];
+
+function writeCards(cards) {
+  for let i = 0, i < cards.length, i++) {
+    console.log('Thank you, , for the wonderful birthday gift!');
+  }
+  return cards;
+}
+
+writeCards(cards);
