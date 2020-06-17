e15297b01d294ce5eaeeb0d09b7ad858133b49a9
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-17 05:57:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const cards = ["Ada", "Brendan", "Ali"];
 
 function writeCards(cards) {
-  for (let i = 0, i < cards.length, i++) {
+  for (let i = 0; i < cards.length; i++) {
     console.log('Thank you, ${cards[i]}, for the wonderful birthday gift!');
   }
   return cards;
